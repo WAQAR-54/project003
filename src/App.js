d2472e7c9d29ce7083968b9502b9807b0d88b602
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-06-14 11:42:09 +0500

    App.js
    Created File

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <b>Created by Waqar Azeem</b>
         </p>
         <a
           className="App-link"
