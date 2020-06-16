a897ce70a969115966ef7ad20c7a6da4e13051bc
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-06-17 00:31:27 +0500

    Create Hello.js

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Hello.css'
+
+
+function Hello({firstname}) {
+  return  <div>Hello World from Hello.js  Updated
+      <p className="name">This is Hello Word File :name :{firstname} </p>
+ </div>
+}
+
+export default Hello;
