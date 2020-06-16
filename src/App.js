d2013e6cb1b33907f61747f07af19ec5be9cc54c
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-06-16 15:09:29 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <b>Created by Waqar Azeem</b>
-        </p>
-        <div>Last Update by Waqar</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(data) {
+  return  <div >Hello World from App.js  Updated
+
+<div>Second Div for print Data {data.name} age={data.age}</div>
+  </div>
 }
 
 export default App;
