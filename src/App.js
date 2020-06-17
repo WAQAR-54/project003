c8143b16e82ed2b1795ac3abeac76b412f063aa7
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-06-18 00:08:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-import Hello from './Hello';
 
-function App({name,age}) {
-  return  <div >
-    Hello World from App.js  Updated
+import CV from './CV';
 
-<div>Second Div for print Data {name} your age={age}</div>
-<br/>
-<Hello firstname={name}></Hello>
+function App() {
+  return  <div>
+
+   
+<CV></CV>
   </div>
 }
 
