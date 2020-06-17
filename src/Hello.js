ddb4a1f63d3f2be3456758bff1a9cef1fbf524f0
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-06-18 00:07:41 +0500

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -5,6 +5,23 @@ import './Hello.css'
 function Hello({firstname}) {
   return  <div>Hello World from Hello.js  Updated
       <p className="name">This is Hello Word File :name :{firstname} </p>
+    
+
+
+
+      <h1>Personal Information</h1>
+      <p className="name2">
+          <p><strong>Date of Birth:</strong>02-06-1997</p>
+          <p><strong>Martial Status:</strong>Singal</p>
+          <p><strong>Relidion:</strong>Islam</p>
+          <p><strong>Nationality:</strong>Pakistani</p>
+          <p><strong>Language:</strong>Urdu</p>
+          <p><strong> Contact No:</strong>03013380729</p>
+          <p><strong>CNIC:</strong></p>
+          <p><strong>Address:</strong></p>
+        
+           </p>
+    
  </div>
 }
 
