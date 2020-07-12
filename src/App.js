e862976f8812e90f36ec2cac8bfa39f6fca19e2a
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-12 19:31:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+//import logo from './logo.svg';
 
 import './App.css';
-
 import CV from './CV';
+import Room from './Room';
 
 function App() {
   return  <div>
 
    
 <CV></CV>
+<Room></Room>
   </div>
 }
 
