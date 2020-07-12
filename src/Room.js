f94eb9d611b7af197c22dfd6ec84575f8468d7e7
WAQAR-54 <56077768+WAQAR-54@users.noreply.github.com>
2020-07-12 19:31:42 +0500

    Create Room.js

---
+++ src/Room.js
@@ -0,0 +1,39 @@
+import React ,{useState} from 'react';
+import './Room.css';
+// //import logo from './logo.svg';
+// import Room from './Room';
+// import './App.css';
+
+function Room() {
+  let [islit, setlit]= useState(false);
+  let [age,setAge]=useState(24);
+ 
+    // function updatelit() {
+    
+    //   console.log("button touchrs")
+    //   setlit(!islit); 
+    // }
+    // function updateage(){
+    //   setAge(++Age);
+    // }
+    //let Hello='Hello'+ islit+'word';
+    //let Hello='Hello ${islit} word';
+
+
+  return (
+    <div className={"room " + (islit? "lit":"dark") }>
+   // <div className={' room  ${islit? "lit": "dark"}'}>
+      This is room Component is  {islit? "lit": "dark" } 
+      <br/>
+      My Name is Waqar Azeem  
+      <br/>
+      My Age :  {age} 
+      <br/>
+      <button onClick={()=> setlit(!islit)} > Toggle Button</button>
+      <br/>
+      <button onClick={()=> setAge(++age)} > Age Shuffle</button>
+    </div>
+  );
+}
+
+export default Room;
